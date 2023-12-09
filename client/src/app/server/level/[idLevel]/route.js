aa676f49f68891d162f822a2f84fbabb73685f3c
migayalach <ayalachavezmiguel@gmail.com>
2023-12-08 23:39:27 -0400

    contiene a delele y get por id

--- client/src/app/server/level/[idLevel]/route.js
+++ client/src/app/server/level/[idLevel]/route.js
@@ -1,13 +1,29 @@
+// METODOS ELIMINAR, EDITAR Y BUSCAR POR ID
 import { NextResponse } from "next/server";
-const URL = `https://pokeapi.co/api/v2/pokemon`;
+import { prisma } from "@/utils/prisma";
+
+// COSAS A CONSIDERAR
+// - MANEJADOR DE ERRORES
+// - RESPUESTAS VACIAS
 
 export const GET = async (request, { params }) => {
-  const searchParams = new URLSearchParams(request.url.split("?")[1]);
-  console.log(searchParams);
-  console.log(searchParams.get("name"));
-  console.log(searchParams.get("number"));
+  const levelSearch = await prisma.level.findUnique({
+    where: {
+      idLevel: +params.idLevel,
+    },
+  });
+  return NextResponse.json({ level: true, levelSearch });
+};
 
-  const characterData = await fetch(`${URL}/${params.idLevel}`);
-  const data = await characterData.json();
-  return NextResponse.json(data);
+export const DELETE = async (request, { params }) => {
+  const deleteLevel = await prisma.level.delete({
+    where: {
+      idLevel: +params.idLevel,
+    },
+  });
+  return NextResponse.json({
+    message: "Level delete success",
+    levelDelete: true,
+    deleteLevel,
+  });
 };
