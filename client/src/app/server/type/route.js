7e1b84d588aecc4ff5c873f503c7f4d61bac91bf
migayalach <ayalachavezmiguel@gmail.com>
2023-12-09 23:29:44 -0400

    obtener get y luego guardar los tipo de datos

---
+++ client/src/app/server/type/route.js
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/utils/prisma";
+const URL_TYPES = `https://pokeapi.co/api/v2/type`;
+
+export const GET = async () => {
+  const response = await fetch(URL_TYPES);
+  const type = (await response.json()).results.map(({ name }) => name);
+  return NextResponse.json({
+    types: true,
+    message: "Se obtuvo una lista con todos los tipos de pokemon",
+    type,
+  });
+};
+
+export const POST = async (request) => {
+  const { type } = await request.json();
+  const countType = await prisma.type.count();
+  if (!countType) {
+    const createType = type.map((index) =>
+      prisma.type.create({ data: { name: index, create: true } })
+    );
+    const response = await Promise.all(createType);
+    return NextResponse.json({
+      create: true,
+      message: "Se agregaron con exito los typos de pokemon",
+      response,
+    });
+  }
+  return NextResponse.json({
+    create: false,
+    message: `La base de datos ya tiene datos guardados`,
+  });
+};
