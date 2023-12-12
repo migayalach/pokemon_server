80d141ba220ca067559a1f4c1aaf7f0f91918529
migayalach <ayalachavezmiguel@gmail.com>
2023-12-11 23:52:43 -0400

    metodo post listo

--- client/src/app/server/pokemon/route.js
+++ client/src/app/server/pokemon/route.js
@@ -1,19 +1,56 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/utils/prisma";
-//postear //no puede hacer dos con el mismo nombre
-//buscar todo, nombre, los que creaste o los de la api
+const { selectDataPokemon, responseGetPokemon } = require("@/utils/functions");
 //editar solo los creados
 //delete solo los creados
 
-export const GET = async () => {
-  return NextResponse.json({
-    message: "GET pokemon",
-  });
+export const GET = async (request) => {
+  const searchParams = new URLSearchParams(request.url.split("?")[1]);
+  let dataUrl = null;
+  if (searchParams.get("name")) {
+    dataUrl = searchParams.get("name");
+    const searchName = await prisma.pokemon.findMany({
+      where: {
+        name: {
+          contains: `%${dataUrl}%`,
+        },
+      },
+      select: selectDataPokemon(),
+    });
+    return NextResponse.json(
+      responseGetPokemon(true, "Get name pokemon", searchName)
+    );
+  } else if (searchParams.get("create")) {
+    const createBoolean = searchParams.get("create") === "true";
+    const pokemonCreate = await prisma.pokemon.findMany({
+      where: {
+        create: createBoolean,
+      },
+      select: selectDataPokemon(),
+    });
+    return NextResponse.json(
+      responseGetPokemon(true, "Get create pokemon", pokemonCreate)
+    );
+  } else {
+    const pokemonAll = await prisma.pokemon.findMany({
+      select: selectDataPokemon(),
+    });
+    return NextResponse.json(
+      responseGetPokemon(true, "Get all pokemon's", pokemonAll)
+    );
+  }
 };
 
 export const POST = async (request) => {
   const { name, height, weight, life, attack, defense, speed, types } =
     await request.json();
+  const countPokemon = await prisma.pokemon.count();
+  if (!countPokemon) {
+    return NextResponse.json({
+      pokemon: false,
+      message: `Lo siento si aun no hay datos de pokemon guardados desde la api no se puede agregar nuevos pokemon´s pongase en contacto con el administrador`,
+    });
+  }
   const duplicateName = await prisma.pokemon.findFirst({
     where: {
       name,
