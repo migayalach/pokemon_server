8e740226dcf4af1eba6dedee6877c898040ce9fd
migayalach <ayalachavezmiguel@gmail.com>
2023-12-12 17:32:28 -0400

    pokemon eliminado

--- client/src/app/server/pokemon/[idPokemon]/route.js
+++ client/src/app/server/pokemon/[idPokemon]/route.js
@@ -9,8 +9,39 @@ export const GET = (request, { params }) => {
   });
 };
 
-export const DELETE = (request, { params }) => {
+export const DELETE = async (request, { params }) => {
+  const existPokemon = await prisma.pokemon.findUnique({
+    where: {
+      idPokemon: +params.idPokemon,
+    },
+  });
+  if (!existPokemon) {
+    return NextResponse.json({
+      pokemonDelete: false,
+      message: "El pokemon que intenta eliminar no se encuentra registrado",
+    });
+  }
+  if (existPokemon.create === false) {
+    return NextResponse.json({
+      pokemonDelete: false,
+      message: "No se puede eliminar el pokemon que selecciono",
+    });
+  }
+
+  await prisma.pokemonType.deleteMany({
+    where: {
+      idPokemon: +params.idPokemon,
+    },
+  });
+
+  await prisma.pokemon.delete({
+    where: {
+      idPokemon: +params.idPokemon,
+    },
+  });
+
   return NextResponse.json({
-    message: "DELETE pokemon",
+    pokemonDelete: true,
+    message: "Pokemon eliminado",
   });
 };
