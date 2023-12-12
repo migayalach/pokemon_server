9ecebe207a608e966b62a62f38c08e609856971f
migayalach <ayalachavezmiguel@gmail.com>
2023-12-11 23:53:14 -0400

    se agrego nuevas funciones para la clase pokemon

--- client/src/utils/functions.js
+++ client/src/utils/functions.js
@@ -1,3 +1,4 @@
+// FUNCIONES PARA USUARIO
 const clearResponseUser = (dataUser) =>
   dataUser.map(({ idUser, idLevel, name, email, level }) => ({
     idUser,
@@ -12,8 +13,61 @@ const randomAtrributes = () => Math.floor(Math.random() * (100 - 60 + 1)) + 60;
 const clearTypeId = (array) =>
   array.map((index) => index.map(({ idType }) => idType)).flat();
 
+// FUNCIONES PARA POKEMON  
+const clearResponsePokemon = (arr) =>
+  arr.map(
+    ({
+      idPokemon,
+      name,
+      height,
+      weight,
+      life,
+      attack,
+      defense,
+      speedtypes,
+      create,
+      types,
+    }) => ({
+      idPokemon,
+      name,
+      height,
+      weight,
+      life,
+      attack,
+      defense,
+      speedtypes,
+      create,
+      types: types.map(({ idType }) => idType),
+    })
+  );
+
+const selectDataPokemon = () => ({
+  idPokemon: true,
+  name: true,
+  height: true,
+  weight: true,
+  life: true,
+  attack: true,
+  defense: true,
+  speed: true,
+  create: true,
+  types: {
+    select: {
+      idType: true,
+    },
+  },
+});
+
+const responseGetPokemon = (response, message, responseData) => ({
+  searchPokemon: response,
+  message,
+  dataResponse: clearResponsePokemon(responseData),
+});
+
 module.exports = {
   clearResponseUser,
   randomAtrributes,
   clearTypeId,
+  selectDataPokemon,
+  responseGetPokemon,
 };
