51c56bd7ec94af0aeadf3f1a8f7a233b8ca15895
migayalach <ayalachavezmiguel@gmail.com>
2023-12-12 20:38:01 -0400

    se muestra todos los favoritos de un usuario

--- client/src/app/server/favorite/[idFavorite]/route.js
+++ client/src/app/server/favorite/[idFavorite]/route.js
@@ -1,26 +1,46 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/utils/prisma";
+const {
+  selectDataPokemon,
+  clearResponsePokemon,
+} = require("@/utils/functions");
 
-// llega el id del usuario y muestra todos sus fav
-export const GET = (request, { params }) => {
-  const favoriteUser = prisma.favorite.findMany({
+export const GET = async (request, { params }) => {
+  const favoriteUser = await prisma.favorite.findMany({
     where: {
-      idUser: +params.idUser,
+      idUser: +params.idFavorite,
     },
   });
-  return NextResponse.json({
-    message: "GET FAVORITE",
-  });
-};
+  if (!favoriteUser.length) {
+    return NextResponse.json({
+      searchFavorite: false,
+      message: `No se pudo encontrar ningun favorito para el usuario buscado`,
+      favoriteUser,
+    });
+  }
 
-export const POST = () => {
-  return NextResponse.json({
-    message: "POST FAVORITE",
-  });
-};
-
-export const DELETE = () => {
-  return NextResponse.json({
-    message: "DELETE FAVORITE",
-  });
+  const pokemon = favoriteUser
+    .map(({ idPokemon }) => idPokemon)
+    .map((idPokemon) =>
+      prisma.pokemon.findUnique({
+        where: {
+          idPokemon,
+        },
+        select: selectDataPokemon(),
+      })
+    );
+  const pokemonResponse = await Promise.all(pokemon);
+  const obj = {
+    idUser: favoriteUser[0].idUser,
+    idFavorite: favoriteUser[0].idFavorite,
+    pokemon: clearResponsePokemon(pokemonResponse),
+  };
+  return NextResponse.json(
+    {
+      searchFavorite: true,
+      message: `Lista de favoritos`,
+      favoriteDataUser: obj,
+    },
+    { status: 200 }
+  );
 };
