eac0b0c173eca4fcb63f7b04960c44960d2fb24e
migayalach <ayalachavezmiguel@gmail.com>
2023-12-13 01:12:14 -0400

    elimando la carpeta api

--- api/src/Controllers/Type/typeControllers.js
+++
@@ -1,9 +0,0 @@
-const { Type } = require("../../db");
-const createType = async (nombre) => {
-  const newType = await Type.create({ nombre });
-  return newType;
-};
-
-module.exports = {
-  createType,
-};
