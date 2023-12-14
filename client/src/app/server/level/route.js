a15985130d8e8a9d95e652b3aa39c2e66e5b1e32
migayalach <ayalachavezmiguel@gmail.com>
2023-12-13 23:57:13 -0400

    se empezo por la modularizacion

--- client/src/app/server/level/route.js
+++ client/src/app/server/level/route.js
@@ -1,22 +1,14 @@
 // METODO MOSTRAR TODO Y CREAR
 import { NextResponse } from "next/server";
 import { prisma } from "@/utils/prisma";
+import { validatePutLevel } from "../middleware";
 
-// COSAS A CONSIDERAR
-// - MANEJADOR DE ERRORES
-// - REPETIDOS
-// - RESPUESTAS VACIAS
-
-export const GET = async (request) => {
-  const searchParams = new URLSearchParams(request.url.split("?")[1]);
-  const dataUrl = searchParams.get("name");
+const getControllerLevel = async (dataUrl) => {
   if (!dataUrl) {
-    const levelList = await prisma.level.findMany();
-    return NextResponse.json({
-      message: "List level",
-      levelList,
-    });
+    const levelResponse = await prisma.level.findMany();
+    return { level: true, responseLebel: levelResponse };
   }
+
   const nameLevel = await prisma.level.findMany({
     where: {
       name: {
@@ -24,29 +16,67 @@ export const GET = async (request) => {
       },
     },
   });
+
   if (nameLevel.length) {
-    return NextResponse.json({
-      levelName: true,
-      nameLevel,
-    });
+    return { level: true, responseLebel: nameLevel };
   }
-  return NextResponse.json({
-    levelName: false,
-    nameLevel,
-  });
+
+  throw Error(`No se pudo encontrar ningun nivel`);
 };
 
-export const POST = async (request) => {
-  const { name } = await request.json();
+const putControllerLevel = async (name) => {
+  const levelExist = await prisma.level.findMany({
+    where: {
+      name,
+    },
+  });
+
+  if (levelExist.length) {
+    return { message: "Ya existe un nivel con este nombre", newLevel: [] };
+  }
+
   const newLevel = await prisma.level.create({
     data: {
       name,
     },
   });
-  return NextResponse.json({
-    message: "Level creado con exito",
-    newLevel,
-  });
+
+  return { message: "Level creado con éxito", newLevel };
+};
+
+export const GET = async (request) => {
+  const searchParams = new URLSearchParams(request.url.split("?")[1]);
+  const dataUrl = searchParams.get("name");
+  try {
+    const { level, responseLebel } = await getControllerLevel(dataUrl);
+    return NextResponse.json({
+      message: "List level",
+      level,
+      responseLebel,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      error: error.message,
+      level: false,
+      responseLebel: [],
+    });
+  }
+};
+
+export const POST = async (request) => {
+  const { name } = await request.json();
+  try {
+    const dataResponse = await validatePutLevel(name, putControllerLevel);
+    return NextResponse.json({
+      dataResponse,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      dataResponse: {
+        message: error.message,
+      },
+    });
+  }
 };
 
 export const PUT = async (request) => {
