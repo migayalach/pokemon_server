235beb6bbeca4506dec7123cfdd4aa11fcac2d93
migayalach <ayalachavezmiguel@gmail.com>
2023-12-15 22:35:16 -0400

    modularizacion completa

--- client/src/app/server/pokemon/[idPokemon]/route.js
+++ client/src/app/server/pokemon/[idPokemon]/route.js
@@ -1,64 +1,31 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/utils/prisma";
-const {
-  clearResponsePokemon,
-  selectDataPokemon,
-} = require("@/utils/functions");
+import { getIdPokemon, deletePokemon } from "../controllerPokemon";
+import {
+  responseSuccessPokemon,
+  responseErrorPokemon,
+} from "@/utils/responseJson";
 
+//devolver pokemon type nombre
 export const GET = async (request, { params }) => {
-  const searchPokemonId = await prisma.pokemon.findUnique({
-    where: {
-      idPokemon: +params.idPokemon,
-    },
-    select: selectDataPokemon(),
-  });
-  if (!searchPokemonId) {
-    return NextResponse.json({
-      pokemonSearch: false,
-      message: "No se pudo encontrar el pokemon que busca",
-    });
+  const idPokemon = +params.idPokemon;
+  try {
+    const pokemonId = await getIdPokemon(idPokemon);
+    return NextResponse.json(
+      responseSuccessPokemon(`Pokemon encontrado`, pokemonId)
+    );
+  } catch (error) {
+    return NextResponse.json(responseErrorPokemon(error.message));
   }
-
-  return NextResponse.json({
-    pokemonSearch: true,
-    message: "GET pokemon",
-    pokemon: clearResponsePokemon([searchPokemonId]),
-  });
 };
 
 export const DELETE = async (request, { params }) => {
-  const existPokemon = await prisma.pokemon.findUnique({
-    where: {
-      idPokemon: +params.idPokemon,
-    },
-  });
-  if (!existPokemon) {
-    return NextResponse.json({
-      pokemonDelete: false,
-      message: "El pokemon que intenta eliminar no se encuentra registrado",
-    });
+  const idPokemon = +params.idPokemon;
+  try {
+    const pokemonDelete = await deletePokemon(idPokemon);
+    return NextResponse.json(
+      responseSuccessPokemon(`Pokemon eliminado`, pokemonDelete)
+    );
+  } catch (error) {
+    return NextResponse.json(responseErrorPokemon(error.message));
   }
-  if (existPokemon.create === false) {
-    return NextResponse.json({
-      pokemonDelete: false,
-      message: "No se puede eliminar el pokemon que selecciono",
-    });
-  }
-
-  await prisma.pokemonType.deleteMany({
-    where: {
-      idPokemon: +params.idPokemon,
-    },
-  });
-
-  await prisma.pokemon.delete({
-    where: {
-      idPokemon: +params.idPokemon,
-    },
-  });
-
-  return NextResponse.json({
-    pokemonDelete: true,
-    message: "Pokemon eliminado",
-  });
 };
