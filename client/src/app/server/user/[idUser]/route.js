fa19095b97b6758e3471c888704a41fe8ee452a3
migayalach <ayalachavezmiguel@gmail.com>
2023-12-15 22:32:41 -0400

    modularizacion completa

--- client/src/app/server/user/[idUser]/route.js
+++ client/src/app/server/user/[idUser]/route.js
@@ -1,57 +1,28 @@
 // METODOS ELIMINAR, EDITAR Y BUSCAR POR ID
 import { NextResponse } from "next/server";
-import { prisma } from "@/utils/prisma";
-import { clearResponseUser } from "@/utils/functions";
+import { getIdUser, deleteUser } from "../controllerUser";
+import { responseSuccessUser, responseErrorUser } from "@/utils/responseJson";
 
 export const GET = async (request, { params }) => {
-  const dataUser = await prisma.user.findUnique({
-    where: {
-      idUser: +params.idUser,
-    },
-    select: {
-      idUser: true,
-      idLevel: true,
-      name: true,
-      email: true,
-      level: {
-        select: {
-          name: true,
-        },
-      },
-    },
-  });
-  if (!dataUser) {
-    return NextResponse.json({
-      user: false,
-      message: `El usuario que intenta buscar no existe`,
-      dataUser,
-    });
+  const idUser = +params.idUser;
+  try {
+    const userDataId = await getIdUser(idUser);
+    return NextResponse.json(
+      responseSuccessUser(`Usuario encontrado`, userDataId)
+    );
+  } catch (error) {
+    return NextResponse.json(responseErrorUser(error.message));
   }
-  return NextResponse.json({
-    user: true,
-    message: "Usuario encontrado",
-    dataUser: clearResponseUser([dataUser]),
-  });
 };
 
 export const DELETE = async (request, { params }) => {
-  const existUser = await prisma.user.findUnique({
-    where: { idUser: +params.idUser },
-  });
-  if (!existUser) {
-    return NextResponse.json({
-      user: false,
-      message: `El usuario que intenta eliminar no existe`,
-    });
+  const idUser = +params.idUser;
+  try {
+    const userDelete = await deleteUser(idUser);
+    return NextResponse.json(
+      responseSuccessUser(`Usuario eliminado con exito`, userDelete)
+    );
+  } catch (error) {
+    return NextResponse.json(responseErrorUser(error.message));
   }
-  await prisma.user.delete({
-    where: {
-      idUser: +params.idUser,
-    },
-  });
-
-  return NextResponse.json({
-    user: true,
-    message: `Usuario eliminado con exito`,
-  });
 };
