96503f184b2f01da466522703e88fbf63f49717e
migayalach <ayalachavezmiguel@gmail.com>
2023-12-15 22:31:02 -0400

    respuestas personalizadas listo

--- client/src/utils/responseJson.js
+++ client/src/utils/responseJson.js
@@ -1,8 +1,10 @@
+const { arrayResponse } = require("./functions");
+
 // RESPONSE LEVEL
 const responseSuccessLevel = (message, dataLevel) => ({
   level: true,
   message,
-  dataLevel: Array.isArray(dataLevel) ? dataLevel : [dataLevel],
+  dataLevel: arrayResponse(dataLevel),
 });
 
 const responseErrorLevel = (message) => ({
@@ -11,7 +13,52 @@ const responseErrorLevel = (message) => ({
   dataLevel: [],
 });
 
+// RESPONSE USER
+const responseSuccessUser = (message, dataUser) => ({
+  user: true,
+  message,
+  dataUser: arrayResponse(dataUser),
+});
+
+const responseErrorUser = (message) => ({
+  user: false,
+  message,
+  dataUser: [],
+});
+
+// RESPONSE TYPE
+const responseSuccessType = (message, dataType) => ({
+  type: true,
+  message,
+  dataType: dataType,
+});
+
+const responseErrorType = (message) => ({
+  type: false,
+  message,
+  dataUser: [],
+});
+
+// RESPONSE POKEMON
+const responseSuccessPokemon = (message, dataPokemon) => ({
+  pokemon: true,
+  message,
+  dataPokemon: arrayResponse(dataPokemon),
+});
+
+const responseErrorPokemon = (message) => ({
+  pokemon: false,
+  message,
+  dataPokemon: [],
+});
+
 module.exports = {
   responseSuccessLevel,
   responseErrorLevel,
+  responseSuccessUser,
+  responseErrorUser,
+  responseSuccessType,
+  responseErrorType,
+  responseSuccessPokemon,
+  responseErrorPokemon,
 };
