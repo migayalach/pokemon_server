55d78283fe3bca1abce78aa04750fc2b14f24707
migayalach <ayalachavezmiguel@gmail.com>
2023-12-15 22:33:00 -0400

    modularizacion completa

--- client/src/app/server/user/route.js
+++ client/src/app/server/user/route.js
@@ -1,103 +1,38 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/utils/prisma";
-import { clearResponseUser } from "@/utils/functions";
+import { createUser, getUser, updateUser } from "./controllerUser";
+import { responseSuccessUser, responseErrorUser } from "@/utils/responseJson";
 
 export const POST = async (request) => {
   const { name, email, password, idLevel } = await request.json();
-  const newUser = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password,
-      idLevel: +idLevel,
-    },
-  });
-  return NextResponse.json({
-    create: true,
-    newUser,
-  });
+  try {
+    const postUser = await createUser(name, email, password, idLevel);
+    return NextResponse.json(
+      responseSuccessUser(`Usuario creado con exito`, postUser)
+    );
+  } catch (error) {
+    return NextResponse.json(responseErrorUser(error.message));
+  }
 };
 
 export const GET = async (request) => {
   const searchParams = new URLSearchParams(request.url.split("?")[1]);
   const dataUrl = searchParams.get("email");
-  if (!dataUrl) {
-    const userData = await prisma.user.findMany({ include: { level: true } });
-    return NextResponse.json({
-      message: "User list",
-      userData: clearResponseUser(userData),
-    });
+  try {
+    const getDataUser = await getUser(dataUrl);
+    return NextResponse.json(responseSuccessUser(`Usuario`, getDataUser));
+  } catch (error) {
+    return NextResponse.json(responseErrorUser(error.message));
   }
-  const dataUser = await prisma.user.findMany({
-    where: {
-      email: dataUrl,
-    },
-    select: {
-      idUser: true,
-      idLevel: true,
-      name: true,
-      email: true,
-      level: {
-        select: {
-          name: true,
-        },
-      },
-    },
-  });
-
-  if (dataUser.length) {
-    return NextResponse.json({
-      userName: true,
-      userData: clearResponseUser(dataUser),
-    });
-  }
-  return NextResponse.json({
-    userName: false,
-    nameUser,
-  });
 };
 
 export const PUT = async (request) => {
   const { idUser, name, email, password, idLevel } = await request.json();
-  const userExist = await prisma.user.findUnique({
-    where: {
-      idUser: +idUser,
-    },
-  });
-  if (!userExist) {
-    return NextResponse.json({
-      user: false,
-      message: `El usuario que intentas editar, no existe`,
-      userExist,
-    });
+  try {
+    const editUser = await updateUser(+idUser, name, email, password, +idLevel);
+    return NextResponse.json(
+      responseSuccessUser(`Actualizacion exitosa`, editUser)
+    );
+  } catch (error) {
+    return NextResponse.json(responseErrorUser(error.message));
   }
-  const levelExist = await prisma.level.findUnique({
-    where: {
-      idLevel: +idLevel,
-    },
-  });
-  if (!levelExist) {
-    return NextResponse.json({
-      user: false,
-      message: `El nivel que intentas editar, no existe`,
-      levelExist,
-    });
-  }
-
-  const editUser = await prisma.user.update({
-    where: {
-      idUser: +idUser,
-    },
-    data: {
-      name,
-      email,
-      password,
-      idLevel: +idLevel,
-    },
-  });
-  return NextResponse.json({
-    user: true,
-    message: "Usuario editado con exito",
-    editUser,
-  });
 };
