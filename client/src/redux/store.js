111f7a9712000c6a09535876c11fc0a448414f37
migayalach <ayalachavezmiguel@gmail.com>
2024-01-02 17:24:01 -0400

    configuracion de la tienda

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
+import { userAPI } from "./services/loginServer";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [userAPI.reducerPath]: userAPI.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([userAPI.middleware]),
 });
+
+setupListeners(store.dispatch);
